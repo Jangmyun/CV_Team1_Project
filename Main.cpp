7772d31415524a7fd45617107d895c41a31673b9
Jangmyun <cndlsrb2739@gmail.com>
2025-11-27 02:25:17 +0900

    feat: motion estimator 설정

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/videostab.hpp>
 
 using namespace std;
 using namespace cv;
+using namespace cv::videostab;
 
 int main(int argc, char** argv) {
   if (argc < 2) {
@@ -12,6 +14,22 @@ int main(int argc, char** argv) {
 
   string inputPath = argv[1];
   string outputPath = (argc >= 3) ? argv[2] : "stabilized_video.avi";
+
+  try {
+    Ptr<VideoFileSource> src = makePtr<VideoFileSource>(inputPath);
+
+    OnePassStabilizer* stabilizer = new OnePassStabilizer();
+
+    Ptr<MotionEstimatorRansacL2> motionEstimator =
+        makePtr<MotionEstimatorRansacL2>(MM_AFFINE);
+
+    Ptr<KeypointBasedMotionEstimator> kBasedEst =
+        makePtr<KeypointBasedMotionEstimator>(motionEstimator);
+
+    kBasedEst->setDetector(GFTTDetector::create(1000));
+
+    stabilizer->setMotionEstimator(kBasedEst);
+  }
 
   return 0;
 }
