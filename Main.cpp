d230616ccf2de1a3c0db63c1943d9b9677117987
Jangmyun <cndlsrb2739@gmail.com>
2025-11-27 02:15:38 +0900

    feat: program argument 받기 (input video name, output video name)

--- Main.cpp
+++ Main.cpp
@@ -4,4 +4,14 @@
 using namespace std;
 using namespace cv;
 
-int main() { return 0; }
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    cout << "Usage: " << argv[0] << " <input_video> [output_video]" << endl;
+    return 1;
+  }
+
+  string inputPath = argv[1];
+  string outputPath = (argc >= 3) ? argv[2] : "stabilized_video.avi";
+
+  return 0;
+}
